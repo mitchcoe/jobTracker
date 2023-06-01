f5ff9c41fcd493b3cac251f5f7d1d375e0f6b3bd
mitchcoe <mitchellcoe09@gmail.com>
2023-06-01 18:39:50 -0500

    add better pagination and add sorting

--- src/JobsContainer/JobsContainer.tsx
+++ src/JobsContainer/JobsContainer.tsx
@@ -1,6 +1,12 @@
 import React, {useState } from 'react'
-import { Tabs, Tab, Box, TablePagination } from '@mui/material';
 import Job from '../Job/Job';
+import { Tabs, Tab, Box, TablePagination, IconButton, MenuItem, Typography } from '@mui/material';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
+import { useTheme } from '@mui/material/styles';
+import FirstPageIcon from '@mui/icons-material/FirstPage';
+import KeyboardArrowLeft from '@mui/icons-material/KeyboardArrowLeft';
+import KeyboardArrowRight from '@mui/icons-material/KeyboardArrowRight';
+import LastPageIcon from '@mui/icons-material/LastPage';
 
 import type { JobType } from '../globalTypes';
 
@@ -8,6 +14,101 @@ type TabPanelProps = {
   children?: React.ReactNode,
   index: number,
   value: number,
+}
+type Order = 'asc' | 'desc';
+type OrderProperty = 'remote' | 'application_date' | 'favorite'
+
+type TablePaginationActionsProps = {
+  count: number;
+  page: number;
+  rowsPerPage: number;
+  onPageChange: (
+    event: React.MouseEvent<HTMLButtonElement>,
+    newPage: number,
+  ) => void;
+}
+
+
+function descendingComparator<T>(a: T, b: T, orderBy: keyof T) {
+  if (b[orderBy] < a[orderBy]) {
+    return -1;
+  }
+  if (b[orderBy] > a[orderBy]) {
+    return 1;
+  }
+  return 0;
+}
+
+function getComparator<Key extends keyof any>(
+  order: Order,
+  orderBy: Key,
+): (
+  a: { [key in Key]: number | string | boolean },
+  b: { [key in Key]: number | string | boolean },
+) => number {
+  return order === 'desc'
+    ? (a, b) => descendingComparator(a, b, orderBy)
+    : (a, b) => -descendingComparator(a, b, orderBy);
+}
+
+function TablePaginationActions(props: TablePaginationActionsProps) {
+  const theme = useTheme();
+  const { count, page, rowsPerPage, onPageChange } = props;
+
+  const handleFirstPageButtonClick = (
+    event: React.MouseEvent<HTMLButtonElement>,
+  ) => {
+    onPageChange(event, 0);
+  };
+
+  const handleBackButtonClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    onPageChange(event, page - 1);
+  };
+
+  const handleNextButtonClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    onPageChange(event, page + 1);
+  };
+
+  const handleLastPageButtonClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    onPageChange(event, Math.max(0, Math.ceil(count / rowsPerPage) - 1));
+  };
+
+  return (
+    <Box sx={{ flexShrink: 0, ml: 2.5 }}>
+      <IconButton
+        onClick={handleFirstPageButtonClick}
+        disabled={page === 0}
+        aria-label="first page"
+        sx={{'&:disabled': {color: 'grey'}, color: 'white'}}
+      >
+        {theme.direction === 'rtl' ? <LastPageIcon /> : <FirstPageIcon />}
+      </IconButton>
+      <IconButton
+        onClick={handleBackButtonClick}
+        disabled={page === 0}
+        aria-label="previous page"
+        sx={{'&:disabled': {color: 'grey'}, color: 'white'}}
+      >
+        {theme.direction === 'rtl' ? <KeyboardArrowRight /> : <KeyboardArrowLeft />}
+      </IconButton>
+      <IconButton
+        onClick={handleNextButtonClick}
+        disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+        aria-label="next page"
+        sx={{'&:disabled': {color: 'grey'}, color: 'white'}}
+      >
+        {theme.direction === 'rtl' ? <KeyboardArrowLeft /> : <KeyboardArrowRight />}
+      </IconButton>
+      <IconButton
+        onClick={handleLastPageButtonClick}
+        disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+        aria-label="last page"
+        sx={{'&:disabled': {color: 'grey'}, color: 'white'}}
+      >
+        {theme.direction === 'rtl' ? <FirstPageIcon /> : <LastPageIcon />}
+      </IconButton>
+    </Box>
+  );
 }
 
 function TabPanel(props: TabPanelProps) {
@@ -38,10 +139,21 @@ function a11yProps(index: number) {
 }
 
 export default function JobsContainer(props: {jobs: JobType[], getJobs: () => void,}) {
+  const [order, setOrder] = useState<Order>('asc');
+  const [orderBy, setOrderBy] = useState< 'remote' | 'application_date' | 'favorite'>('application_date');
   const [page, setPage] = useState(0);
-  const [rowsPerPage, setRowsPerPage] = React.useState(5);
-  const [value, setValue] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(5);
+  const [value, setValue] = useState(0);
   const { jobs, getJobs } = props
+
+  const handleRequestSort = (
+    event: SelectChangeEvent,
+  ) => {
+    const property: 'remote' | 'application_date' | 'favorite' = event.target.value as OrderProperty
+    const isAsc = orderBy === property && order === 'asc';
+    setOrder(isAsc ? 'desc' : 'asc');
+    setOrderBy(property);
+  };
 
   // const testJobs = jobs && Array(6).fill(jobs[0])
   const handleChange = (_event: React.SyntheticEvent, newValue: number) => {
@@ -66,7 +178,7 @@ export default function JobsContainer(props: {jobs: JobType[], getJobs: () => vo
         </Tabs>
       </Box>
       <TabPanel value={value} index={0}>
-        {jobs.filter((job) => !job.archived).slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((job, key) => (
+        {jobs.filter((job) => !job.archived).slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).sort(getComparator(order, orderBy)).map((job, key) => (
           <Job job={job} key={key + job.title} getJobs={getJobs}/>
         ))}
       </TabPanel>
@@ -75,6 +187,28 @@ export default function JobsContainer(props: {jobs: JobType[], getJobs: () => vo
           <Job job={job} key={key + job.title} getJobs={getJobs}/>
         ))}
       </TabPanel>
+      <div style={{display: 'flex', alignItems: 'center', justifyContent: 'flex-end'}}>
+        <Typography>Sort By:</Typography>
+        <Select
+          value={orderBy}
+          onChange={handleRequestSort}
+          sx={{color: 'white', '& fieldset': {border: 'none'}, '& svg': {color: 'white'}}}
+        >
+          {/* <MenuItem value="salary_range">
+            Salary
+          </MenuItem> */}
+          <MenuItem value="remote">
+            Remote Position
+          </MenuItem>
+          <MenuItem value="application_date">
+            Application Date
+          </MenuItem>
+          <MenuItem value="favorite">
+            Favorites
+          </MenuItem>
+        </Select>
+        <Typography>{order === 'asc' ? 'Asc' : 'Desc'}</Typography>
+      </div>
       <TablePagination
         component="div"
         count={jobs.length}
@@ -83,10 +217,11 @@ export default function JobsContainer(props: {jobs: JobType[], getJobs: () => vo
         onPageChange={handleChangePage}
         onRowsPerPageChange={handleChangeRowsPerPage}
         rowsPerPageOptions={[5, 10, 25, 100]}
+        ActionsComponent={TablePaginationActions}
         sx={{
           color: 'white',
           '& .MuiTablePagination-actions button': {color: 'white'},
-          '& .MuiTablePagination-selectIcon': {color: 'white'}
+          '& .MuiTablePagination-selectIcon': {color: 'white'},
         }}
       />
     </Box>
